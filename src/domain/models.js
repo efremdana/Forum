b1f24f8a077e34eaad333cc904686fcecb0518e1
efremdana <efremenkodana@gmail.com>
2025-02-16 19:11:22 +0300

    Deleted extra import and export

--- src/domain/models.js
+++ src/domain/models.js
@@ -1,8 +1,7 @@
 import Article from './articles/model.js';
 import Comment from './comments/model.js';
-import sequelize from './db.js';
 
 Article.hasMany(Comment, { foreignKey: 'article_id', onDelete: 'CASCADE' });
 Comment.belongsTo(Article, { foreignKey: 'article_id' });
 
-export { sequelize, Article, Comment };
+export { Article, Comment };
