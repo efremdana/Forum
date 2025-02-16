cec3ec01d0845c061a04ecde3437ca795daf4531
efremdana <efremenkodana@gmail.com>
2025-02-16 20:47:06 +0300

    Add alias for relationship between article and comment models

--- src/domain/models.js
+++ src/domain/models.js
@@ -1,7 +1,7 @@
 import Article from './articles/model.js';
 import Comment from './comments/model.js';
 
-Article.hasMany(Comment, { foreignKey: 'article_id', onDelete: 'CASCADE' });
-Comment.belongsTo(Article, { foreignKey: 'article_id' });
+Article.hasMany(Comment, { foreignKey: 'article_id', onDelete: 'CASCADE', as: 'comments' });
+Comment.belongsTo(Article, { foreignKey: 'article_id', as: 'article' });
 
 export { Article, Comment };
