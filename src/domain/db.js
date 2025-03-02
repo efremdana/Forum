9ac8b5bb2a48340733817543cd56775afefed661
efremdana <efremenkodana@gmail.com>
2025-03-02 19:33:20 +0300

    Init frontend vue js

--- src/domain/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('Forum', 'postgres', 'farveh8', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false,
-});
-
-export default sequelize;
