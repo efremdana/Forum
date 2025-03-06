68734d2d2456da2afeeeaaab97be5b82801bde74
efremdana <efremenkodana@gmail.com>
2025-03-06 22:31:38 +0300

    Add component list comments

---
+++ frontend/src/components/ListComments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mt-4 space-y-4">
+    <div
+      v-for="(comment, index) in listComments"
+      :key="index"
+      class="bg-gray-100 p-4 rounded-lg flex justify-between items-center"
+    >
+      <p class="text-gray-700">{{ comment }}</p>
+      <div class="space-x-2">
+        <button
+          @click="$emit('del', index)"
+          class="px-3 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600"
+        >
+          Удалить
+        </button>
+        <button
+          @click="$emit('edit', { text: comment, index: index })"
+          class="px-3 py-1 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600"
+        >
+          Редактировать
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['listComments'],
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
