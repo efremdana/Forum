78ca079a7b063d35f2933c96e83f6e87b5812e6e
efremdana <efremenkodana@gmail.com>
2025-03-09 23:41:35 +0300

    Add vuex store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { router } from './router.js'
+import store from './store.js'
 import App from './App.vue'
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app')
