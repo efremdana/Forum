e5c6af0d8bbb5331ac0c7b1d03f0e8111a1675c6
efremdana <efremenkodana@gmail.com>
2025-03-11 20:54:16 +0300

    Сonfigured cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import articleRouter from "./routes/article/crud.js";
 import commentRouter from "./routes/comment/crud.js";
 import filterRouter from "./routes/filter/routes.js";
@@ -7,6 +8,11 @@ const PORT = 5000
 
 const app = new express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 app.use("/", articleRouter);
 app.use("/article", commentRouter);
