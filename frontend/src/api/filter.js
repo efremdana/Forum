9c5fa490b1374e8e09732a69be4c73abaca07230
efremdana <efremenkodana@gmail.com>
2025-03-11 20:59:55 +0300

    Rename params data request

--- frontend/src/api/filter.js
+++ frontend/src/api/filter.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const getCommentsAnalytics = async (dateFrom, dateTo) => {
   try {
-    const fromDate = new Date(dateFrom);
-    const toDate = new Date(dateTo);
+    const fromDate = new Date(dateFrom)
+    const toDate = new Date(dateTo)
 
     if (isNaN(fromDate.getTime()) || isNaN(toDate.getTime())) {
-      throw new Error('Invalid date format. Use ISO 8601 (e.g., "2024-02-16T12:00:00Z").');
+      throw new Error('Invalid date format. Use ISO 8601 (e.g., "2024-02-16T12:00:00Z").')
     }
 
     if (fromDate > toDate) {
-      throw new Error('dateFrom must be earlier than or equal to dateTo.');
+      throw new Error('dateFrom must be earlier than or equal to dateTo.')
     }
 
     // Выполнение запроса
@@ -19,11 +19,11 @@ export const getCommentsAnalytics = async (dateFrom, dateTo) => {
         dateFrom,
         dateTo,
       },
-    });
+    })
 
-    return response.data;
+    return response.data
   } catch (error) {
-    console.error('Error fetching comments analytics:', error);
-    throw error;
+    console.error('Error fetching comments analytics:', error)
+    throw error
   }
-};
+}
