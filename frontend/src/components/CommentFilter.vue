516c37ced286cd24b49ff38c675719a6f9607c7e
efremdana <efremenkodana@gmail.com>
2025-03-12 23:48:32 +0300

    Refactoring component comment filter

--- frontend/src/components/CommentFilter.vue
+++ frontend/src/components/CommentFilter.vue
@@ -2,7 +2,15 @@
   <div class="mb-4 flex space-x-2">
     <input v-model="startDate" type="date" class="border p-2 rounded" />
     <input v-model="endDate" type="date" class="border p-2 rounded" />
-    <button @click="applyFilter" class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+    <button
+      @click="applyFilter"
+      :disabled="!isValidDateRange"
+      class="p-2 rounded text-white transition-colors"
+      :class="{
+        'bg-blue-500 hover:bg-blue-600': isValidDateRange,
+        'bg-gray-300 cursor-not-allowed': !isValidDateRange,
+      }"
+    >
       Фильтровать
     </button>
   </div>
@@ -13,12 +21,37 @@ export default {
   data() {
     return {
       startDate: '',
-      endDate: '',
+      endDate: ''
     }
   },
+
+  computed: {
+  //   localStartDate: {
+  //     get() {
+  //       return this.startDate
+  //     },
+  //     set(value) {
+  //       this.$emit('update:startDate', value)
+  //     },
+  //   },
+  //   localEndDate: {
+  //     get() {
+  //       return this.endDate
+  //     },
+  //     set(value) {
+  //       this.$emit('update:endDate', value)
+  //     },
+  //   },
+    isValidDateRange() {
+      return this.startDate && this.endDate && this.startDate <= this.endDate
+    },
+  },
+
   methods: {
     applyFilter() {
-      this.$emit('filter', { startDate: this.startDate, endDate: this.endDate })
+      if (this.isValidDateRange) {
+        this.$emit('filter', { startDate: this.startDate, endDate: this.endDate })
+      }
     },
   },
 }
